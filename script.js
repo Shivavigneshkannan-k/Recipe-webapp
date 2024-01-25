1e4828dddb1ec6feea51e9395872797b73e83e37
Shivavigneshkannan-k <shivavigneshml@gmail.com>
2024-01-25 16:20:21 +0530

    Search feature implemented

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const URL_NAME='https:www.themealdb.com/api/json/v1/1/search.php?s=steak';
+const URL_NAME='https:www.themealdb.com/api/json/v1/1/search.php?s=';
 const URL_FIRST_LETTER="https:www.themealdb.com/api/json/v1/1/search.php?f=a";
 const URL_ID="https:www.themealdb.com/api/json/v1/1/lookup.php?i=52772";
 const URL_RANDOM="https:www.themealdb.com/api/json/v1/1/random.php";
@@ -8,22 +8,23 @@ const mealOfTheDayImg= document.querySelector('.meal-of-the-day img');
 const mealOfTheDayDescription= document.querySelector('.meal-of-the-day p');
 const mealOfTheDayDiv= document.querySelector('.hero');
 const like=document.querySelectorAll('like');
-
 const cookNowBtn=document.querySelector('.cook-now');
 const Recipe_foodName=document.querySelector('.food-Name');
 const Recipe_img=document.querySelector('.recipe-img');
 const Recipe_table=document.querySelector('.ingredients');
 const Recipe_instructions=document.querySelector('.instructions');
-
 const tableData=document.querySelectorAll('td')
-
 const homePage=document.querySelector('.container');
 const menu=document.querySelector('.recipe');
 
+const input=document.querySelector('input');
+const search=document.querySelector('.input-button button');
+const searchDiv=document.querySelector('.search');
 
 
 
 const showMenu=(foodData)=>{
+    Recipe_foodName.innerText=foodData["strMeal"];
     Recipe_img.src=foodData["strMealThumb"];
     for(let i=0;i<20;i++){
         const newData1=document.createElement('td');
@@ -56,6 +57,8 @@ const showMenu=(foodData)=>{
     }
     }
 }
+
+
 const mealOfTheDay=async(callback)=>{
     const food = await fetch(URL_RANDOM);
     const data = await food.json();
@@ -68,9 +71,6 @@ const mealOfTheDay=async(callback)=>{
     // }
     callback(foodData);
 }
-
-
-
 const favoriteMeal=(element)=>{
     
     if(element.classList.contains("not-liked")){
@@ -89,9 +89,63 @@ const revealMenu=()=>{
     homePage.classList.replace("unhidden","hidden");
 }
 const closeMenu=()=>{
+    mealOfTheDay(showMenu);
     menu.classList.replace("unhidden",'hidden');
     homePage.classList.replace("hidden","unhidden");
 }
+
+const searchFood = async ()=>{
+    let name=input.value;
+    if(name.trim().length==0){
+        searchDiv.innerHTML="";
+        return;
+    }
+    let data=await fetch(URL_NAME+name);
+    let foodData=await data.json();
+    if(foodData["meals"]==null){
+        searchDiv.innerHTML="<p>No result found!</p>";
+        return 0;
+    }
+    searchDiv.innerHTML=`<p class="result">No of result: ${foodData["meals"].length}</p>`
+    
+    for(let name in foodData){
+        for(let food of foodData[name]){
+
+            const foodDiv=document.createElement('div');
+            foodDiv.classList.add('searched_food');
+
+            const foodImg = document.createElement('img');
+            const foodName = document.createElement('p');
+
+            foodImg.src=food["strMealThumb"];
+            foodName.innerText = food["strMeal"];
+            foodImg.alt=`${food["strMeal"]}`;
+            foodDiv.append(foodImg);
+            foodDiv.append(foodName);
+            searchDiv.append(foodDiv);
+            // console.log(foodData[name]);
+            // const showConsole=()=>console.log(food);
+            // foodDiv.onclick=showConsole;
+            foodDiv.onclick=()=>{
+                // console.log("working")
+                showMenu(food);
+                revealMenu();
+                
+            }
+
+
+        }
+
+    }    
+}
+cookNowBtn.addEventListener('click',()=>{revealMenu()})
+input.addEventListener('blur',()=>{ searchDiv.innerHTML="";})
+search.addEventListener('click',searchFood);
+input.addEventListener('keypress',(e)=>{
+    if(e.key=="Enter"){
+        searchFood()
+    }
+})
 
 mealOfTheDay(showMenu);
 favoriteMeal();
